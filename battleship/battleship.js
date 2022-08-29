2a0d74b2d30d6c67ad3c30308779c098bff39c6d
Polina Lepikhina <lepikhina_p@mail.ru>
2022-08-29 12:07:46 +0400

    battleship page added

---
+++ battleship/battleship.js
@@ -0,0 +1,29 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var shot; //undefined
+var hits = 0;
+var shots = 0;
+
+var isSunk = false;
+
+while (isSunk == false){
+	shot = prompt("Are you ready for shooting? (Enter a number from 0 to 6):");
+	if (shot < 0 || shot > 6){
+		alert("Incorrect number! True again");
+	} else {
+		shots = shots + 1;
+		if (shot == location1 || shot == location2 || shot == location3){
+			hits = hits + 1;
+			if (hits == 3){
+				isSunk = true;
+				alert("You've won! 🎊");
+			}
+		} else {
+			alert("You missed ☹")
+		}
+	}
+}
+
+var status = "You shot " + shots + "times. Your accuracy level is " + (3/shots*100%)
